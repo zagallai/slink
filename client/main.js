b256350b2a0f4611fb27612e319361adc22f5fa9
Amin Zagallai <amza@syntronic.com>
2018-06-01 11:52:31 -0400

    display links in console

--- client/main.js
+++ client/main.js
@@ -1,14 +1,20 @@
 import { Meteor } from 'meteor/meteor'
 import ReactDOM from 'react-dom'
 import { Tracker } from 'meteor/tracker'
+import { Mongo } from 'meteor/mongo'
 
 import { routes, onAuthChange } from './../imports/routes/routes'
-import { links } from './../imports/api/links'
+import { Links } from './../imports/api/links'
 
 Tracker.autorun(() => {
     //double not to make sure truthy data
     const isAuthenticated = !!Meteor.userId()
     onAuthChange(isAuthenticated)
+})
+
+Tracker.autorun(() => {
+    const links = Links.find({}).fetch()
+    console.log(links)
 })
 
 Meteor.startup(() => {
