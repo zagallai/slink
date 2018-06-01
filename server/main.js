d2d9a7f7b9341858ca5eaec224bbd75c901827a2
Amin Zagallai <amza@syntronic.com>
2018-06-01 16:45:59 -0400

    adding shortid

--- server/main.js
+++ server/main.js
@@ -1,8 +1,35 @@
 import { Meteor } from 'meteor/meteor'
+import { WebApp } from 'meteor/webapp'
+
 import './../imports/api/users'
-import './../imports/api/links'
+import { Links } from './../imports/api/links'
 import './../imports/startup/simple-schema-configuration'
 
 Meteor.startup(() => {
-    // code to run on server at startup
+
+    WebApp.connectHandlers.use((req, res, next) => {
+        const _id = req.url.slice(1)
+        const link = Links.findOne({ _id })
+
+        if (link) {
+            res.statusCode = 302
+            res.setHeader('Location', link.url)
+            res.end()
+        } else {
+            next()
+        }
+
+    })
+
+    WebApp.connectHandlers.use((req, res, next) => {
+        console.log('this is from my custom middleware')
+        console.log(req.url, req.method, req.headers, req.query)
+
+        // res.statusCode = 404
+        // res.setHeader('my-custom-header', 'Amin was here')
+        // res.write('<h1>This is my middle at work!</h1>')
+        // res.end()
+
+        next()
+    })
 })
